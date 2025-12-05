0c00e70db14d23364ee30645181f22856fc64228
Hyunsun Chung <horace528@gmail.com>
2025-12-06 01:32:55 +0900

    remove server rendering

--- astro.config.ts
+++ astro.config.ts
@@ -5,21 +5,7 @@ import expressiveCode from 'astro-expressive-code';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import spectre from './package/src';
-
-import node from '@astrojs/node';
 import { spectreDark } from './src/ec-theme';
-
-const {
-  GISCUS_REPO,
-  GISCUS_REPO_ID,
-  GISCUS_CATEGORY,
-  GISCUS_CATEGORY_ID,
-  GISCUS_MAPPING,
-  GISCUS_STRICT,
-  GISCUS_REACTIONS_ENABLED,
-  GISCUS_EMIT_METADATA,
-  GISCUS_LANG
-} = loadEnv(process.env.NODE_ENV!, process.cwd(), "");
 
 // https://astro.build/config
 const config = defineConfig({
@@ -50,10 +36,7 @@ const config = defineConfig({
         }
       },
     })
-  ],
-  adapter: node({
-    mode: 'standalone'
-  })
+  ]
 });
 
 export default config
